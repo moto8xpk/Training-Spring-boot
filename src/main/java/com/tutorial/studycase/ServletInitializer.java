9945029dca3dee3953e57f79e0f3b0516b533a66
moto8xpk <35144476+moto8xpk@users.noreply.github.com>
2020-08-16 15:48:24 +0700

    update yaml file configuration

--- src/main/java/com/tutorial/studycase/ServletInitializer.java
+++ src/main/java/com/tutorial/studycase/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(StudyCaseApplication.class);
+        return application.sources(StudyCaseApplication.class).properties("application.yml");
     }
 
 }
--- src/main/java/com/tutorial/studycase/StudyCaseApplication.java
+++ src/main/java/com/tutorial/studycase/StudyCaseApplication.java
@@ -1,8 +1,6 @@
 package com.tutorial.studycase;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +12,7 @@ public class StudyCaseApplication implements CommandLineRunner {
 
     @Autowired
     RestTemplate restTemplate;
+
 
     public static void main(String[] args) {
 
