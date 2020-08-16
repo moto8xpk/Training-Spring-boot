1014cc7f2a722944c5f53fccac27da9a1f3b708e
moto8xpk <35144476+moto8xpk@users.noreply.github.com>
2020-08-17 01:29:41 +0700

    api create and get all

---
+++ src/main/java/com/tutorial/studycase/ConfigurationSevletInit.java
@@ -0,0 +1,15 @@
+package com.tutorial.studycase;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigurationSevletInit extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+}
--- src/main/java/com/tutorial/studycase/controller/BlogController.java
+++ src/main/java/com/tutorial/studycase/controller/BlogController.java
@@ -1,14 +1,51 @@
 package com.tutorial.studycase.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tutorial.studycase.domain.Blog;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/blog")
 public class BlogController {
 
+    private static Map<String, Blog> blogRepo = new HashMap<>();
+    static {
+        Blog honey = new Blog();
+        honey.setId((long) 1);
+        honey.setName("Honey");
+        blogRepo.put(honey.getId().toString(), honey);
+
+        Blog almond = new Blog();
+        almond.setId((long) 2);
+        almond.setName("Almond");
+        blogRepo.put(almond.getId().toString(), almond);
+    }
+
     @RequestMapping(value = "/hello")
     public String hello() {
         return "Hello World";
     }
+
+    @RequestMapping(value = "/blogs")
+    public ResponseEntity<Object> getBlogs() {
+        return new ResponseEntity<>(blogRepo.values(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/blogs", method = RequestMethod.POST)
+    public ResponseEntity<Object> createBlog(@RequestBody Blog blog) {
+        blogRepo.put(blog.getId().toString(), blog);
+        System.out.println(blog);
+        return new ResponseEntity<>("blog is created successfully", HttpStatus.CREATED);
+    }
+
+//    public ResponseEntity<Object> updateProduct(@PathVariable("id") String id) {
+//    }
+//
+//    public ResponseEntity<Object> getProduct(
+//            @RequestParam(value = "name", required = false, defaultValue = "honey") String name) {
+//    }
 }
