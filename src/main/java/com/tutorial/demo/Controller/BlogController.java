f32d76718645a741d4fb343d9b5f1584dcbc9784
moto8xpk <35144476+moto8xpk@users.noreply.github.com>
2020-08-22 01:02:25 +0700

    interceptor

---
+++ src/main/java/com/tutorial/demo/Controller/BlogController.java
@@ -0,0 +1,29 @@
+package com.tutorial.demo.Controller;
+
+import com.tutorial.demo.Model.Blog;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class BlogController {
+    private static Map<String, Blog> blogRepo = new HashMap<>();
+    static {
+        Blog honey = new Blog();
+        honey.setId("1");
+        honey.setName("Honey");
+        blogRepo.put(honey.getId(), honey);
+        Blog almond = new Blog();
+        almond.setId("2");
+        almond.setName("Almond");
+        blogRepo.put(almond.getId(), almond);
+    }
+    @RequestMapping(value = "/blogs")
+    public ResponseEntity<Object> getBlogs() {
+        return new ResponseEntity<>(blogRepo.values(), HttpStatus.OK);
+    }
+}
