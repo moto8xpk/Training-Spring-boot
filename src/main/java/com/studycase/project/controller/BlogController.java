ec362d75300efb6ea831ae6cdf4e362ddd04dab2
moto8xpk <35144476+moto8xpk@users.noreply.github.com>
2020-09-06 12:43:30 +0700

    create branch thymeleaf for duongWeb

--- src/main/java/com/studycase/project/controller/BlogController.java
+++ src/main/java/com/studycase/project/controller/BlogController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BlogController {
-    @RequestMapping(value = "/")
-    public String hello() {
-        return "Hello World";
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "index";
     }
 }
