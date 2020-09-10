073fcd6b0278dece86e8aa2e94026243d3d93531
moto8xpk <35144476+moto8xpk@users.noreply.github.com>
2020-09-11 01:45:02 +0700

    commit cloud config client

--- src/main/java/com/studycase/project/ProjectApplication.java
+++ src/main/java/com/studycase/project/ProjectApplication.java
@@ -1,10 +1,13 @@
 package com.studycase.project;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 public class ProjectApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
--- src/main/java/com/studycase/project/controller/BlogController.java
+++ src/main/java/com/studycase/project/controller/BlogController.java
@@ -1,12 +1,18 @@
 package com.studycase.project.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class BlogController {
+    @Value("${text.greeting}")
+    String welcomeText;
+
     @RequestMapping(value = "/")
-    public String hello() {
-        return "Hello World";
+    public String welcomeText() {
+        return welcomeText;
     }
 }
