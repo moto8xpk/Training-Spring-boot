29cd5b261fb6fdfee49fb33e8dc7060b210e3cda
moto8xpk <35144476+moto8xpk@users.noreply.github.com>
2020-09-11 02:16:09 +0700

    commit admin server

--- src/main/java/com/studycase/project/ProjectApplication.java
+++ src/main/java/com/studycase/project/ProjectApplication.java
@@ -1,9 +1,12 @@
 package com.studycase.project;
 
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class ProjectApplication {
 
     public static void main(String[] args) {
