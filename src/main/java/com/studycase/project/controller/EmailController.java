ef9fab346be78f7980090b2d2da455d0135e55b4
moto8xpk <35144476+moto8xpk@users.noreply.github.com>
2020-09-18 01:04:12 +0700

    send mail commit

---
+++ src/main/java/com/studycase/project/controller/EmailController.java
@@ -0,0 +1,53 @@
+package com.studycase.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+@RestController
+public class EmailController {
+    @RequestMapping(value = "/sendemail")
+    public String sendEmail() throws IOException, MessagingException,AddressException {
+        sendmail();
+        return "Email sent successfully";
+    }
+    private void sendmail() throws AddressException, MessagingException, IOException {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("lakhongpo@gmail.com", "89541926Aa");
+            }
+        });
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress("lakhongpo@gmail.com", false));
+
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("nhvankhanh199@gmail.com"));
+        msg.setSubject("Tutorials point email");
+        msg.setContent("Tutorials point email", "text/html");
+        msg.setSentDate(new Date());
+
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent("Yeu van khanh nhieu lam !", "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+        MimeBodyPart attachPart = new MimeBodyPart();
+
+        File file=new File("image19.png");
+        attachPart.attachFile(file);
+        multipart.addBodyPart(attachPart);
+        msg.setContent(multipart);
+        Transport.send(msg);
+    }
+}
